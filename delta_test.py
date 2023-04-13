5da1dced195ff1a3f85ab6d77019b370dbad80d8
DISProject <drollfilms@gmail.com>
2023-04-13 09:39:31 +0000

    Added changes to preprocessing and so On

--- delta_test.py
+++ delta_test.py
@@ -1,20 +1,18 @@
-import findspark
-findspark.init("/usr/local/spark")
+#import findspark
+#findspark.init("/usr/local/spark")
 
 from pyspark.sql import SparkSession 
 import pyspark.sql.functions as F 
 
 from delta import *
 
-builder = SparkSession.builder.appName("MyApp") \
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+builder = SparkSession.builder.appName("MydeltaApp")
 
 spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
 business=spark.read.format("parquet").load('hdfs://namenode:9000/project_data/data/business/')
 print(type(business))
-business.write.format("delta").mode("overwrite").save("/temp/business")
+deltaTable = DeltaTable.convertToDelta(spark, "parquet.`hdfs://namenode:9000/project_data/data/business/`")
 
-# df = spark.read.format("delta").load("/temp/business")
-# df.show(5)
+df = spark.read.format("delta").load("/temp/business")
+df.show(5)
